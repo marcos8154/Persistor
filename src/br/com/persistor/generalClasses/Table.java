555c834021d835531af4c03f9658f6e8d217b48e
marcosvinicius <marcosvinicius@192.168.0.100>
2016-07-10 19:55:32 -0300

    Adicionado compatibilidade com FirebirdSQL

---
+++ src/br/com/persistor/generalClasses/Table.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.persistor.generalClasses;
+
+/**
+ *
+ * @author marcosvinicius
+ */
+public class Table {
+
+    
+    public static void create(Object obj)
+    {
+        try
+        {
+            
+        }catch(Exception ex)
+        {
+            
+        }
+    }
+}
